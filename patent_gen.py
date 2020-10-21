877ecf3cb55c05ac19c9c5e4841ba4b98ca0bb89
AKislicyn <alexey.kislicyn@firstlinesoftware.com>
2020-10-21 09:30:46 +0300

    update logging method update scan_project method (add prescan block)

--- patent_gen.py
+++ patent_gen.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import datetime
+import time
 
 parser = argparse.ArgumentParser(
     prog='Patent files generator',
@@ -8,7 +9,7 @@ parser = argparse.ArgumentParser(
     description='Script for converting code '
                 'into document format for subsequent licensing.'
 )
-parser.add_argument('-p', '--prescan', type=str, help='Pre-scan the project.')
+parser.add_argument('-p', '--prescan', type=str, help='Pre-scan the project.', default=1)
 parser.add_argument('-i', '--input', type=str, help='Input project directory.', default='D:\Projects\ironcatbot')
 parser.add_argument('-o', '--output', type=str, help='Output project directory.', default='D:\Python\out')
 parser.add_argument('-l', '--log', type=str, help='Log file path.', default=1)
@@ -21,18 +22,37 @@ args = parser.parse_args()
 
 def scan_project() -> None:
     folders = []
-    for i in os.walk(args.input):
-        folders.append(i)
-    print(folders)
+    files = {}
+    for folder in os.walk(args.input):
+        folders.append(folder)
+
+    if args.prescan == 1:
+        for folder in folders:
+            for file in folder[2]:
+                f_name, f_ext = os.path.splitext(file)
+
+                if f_ext == '':
+                    f_ext = 'empty extension'
+
+                if files.get(f_ext) is None:
+                    files[f_ext] = 1
+                else:
+                    files[f_ext] = files[f_ext] + 1
+
+        for ext in files:
+            # print(ext, files[ext], sep='\t\t\t', end='\n')
+            print("{:<20}{:10d}".format(ext, files[ext]))
+
+    # print(folders)
 
 
 def logging(string) -> None:
     now = datetime.datetime.now()
-    logdateformat = '%d-%m-%Y %H:%M:%S'
+    log_date_format = '%d-%m-%Y %H:%M:%S'
 
     if args.log == 1:
         log = open(args.output + '\\' + 'patent_gen.log', 'a+')
-        log.write(str(now.strftime(logdateformat)) + ' ' + string + '\n')
+        log.write(str(now.strftime(log_date_format)) + ' ' + string + '\n')
         log.close()
 
 
@@ -49,9 +69,15 @@ def prepare():
 
 
 def run():
+    # TODO delete start_time
+    start_time = time.time()
+
     print('Running the project file converter...')
     prepare()
-    # scan_project()
+    scan_project()
+    # TODO delete print end_time
+    print("--- %s seconds ---" % (time.time() - start_time))
+
     print('End.')
     exit(0)
 
