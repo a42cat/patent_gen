7200d48a655483f40dd500d6d9133f87d46508c5
AKislicyn <alexey.kislicyn@firstlinesoftware.com>
2020-10-21 08:16:42 +0300

    add logging method

--- patent_gen.py
+++ patent_gen.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import datetime
 
 parser = argparse.ArgumentParser(
     prog='Patent files generator',
@@ -8,9 +9,9 @@ parser = argparse.ArgumentParser(
                 'into document format for subsequent licensing.'
 )
 parser.add_argument('-p', '--prescan', type=str, help='Pre-scan the project.')
-parser.add_argument('-i', '--input', type=str, help='Input project directory.', default='')
-parser.add_argument('-o', '--output', type=str, help='Output project directory.', default='')
-parser.add_argument('-l', '--log', type=str, help='Log file path.', default=False)
+parser.add_argument('-i', '--input', type=str, help='Input project directory.', default='D:\Projects\ironcatbot')
+parser.add_argument('-o', '--output', type=str, help='Output project directory.', default='D:\Python\out')
+parser.add_argument('-l', '--log', type=str, help='Log file path.', default=1)
 parser.add_argument('-v', '--verbose', action='count', help='Verbose input.', default=1)
 parser.add_argument('--exclude', type=str, help='Exclude file formats.')
 parser.add_argument('--include', type=str, help='Include file formats.')
@@ -19,11 +20,20 @@ args = parser.parse_args()
 
 
 def scan_project() -> None:
-
     folders = []
     for i in os.walk(args.input):
         folders.append(i)
     print(folders)
+
+
+def logging(string) -> None:
+    now = datetime.datetime.now()
+    logdateformat = '%d-%m-%Y %H:%M:%S'
+
+    if args.log == 1:
+        log = open(args.output + '\\' + 'patent_gen.log', 'a+')
+        log.write(str(now.strftime(logdateformat)) + ' ' + string + '\n')
+        log.close()
 
 
 def prepare():
@@ -35,13 +45,13 @@ def prepare():
         print('Enter path to output files...')
         args.output = input()
         print('Path to output files: %s' % args.output)
-    print(args)
+    # print(args)
 
 
 def run():
     print('Running the project file converter...')
     prepare()
-    #scan_project()
+    # scan_project()
     print('End.')
     exit(0)
 
